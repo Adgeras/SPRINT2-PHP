adb6e69174f15148be91ab7db1110096b7ff5625
Edgariukstis <edgaras.banaitis@gmail.com>
2020-08-08 14:00:47 +0300

    employee update is working

--- functions/update.php
+++ functions/update.php
@@ -1,22 +1,19 @@
 <?php
-$oldNameID = $_POST['oldid'];
-if (isset($oldNameID)) {
-    print_r($oldNameID);
-    require_once '../config/connect.php';
-    echo '<h1>Prisijungta</h1>';
+require_once '../config/connect.php';
+echo '<h1>Prisijungta</h1>';
 
-    $NEWname = $_POST['NEWname'];
-    echo ($NEWname) . "<br>";
 
-    //print_r($name);
-    $project = $_POST['projectName'];
+$id = $_POST['id'];
+print $id . "<br>";
+$name = $_POST['name'];
+print $name;
 
-    if (isset($NEWname)) {
-        mysqli_query($connect, "UPDATE `darbuotojai`.`ddarbuotojai` SET `Darbuotojo_Vardas` = $NEWname WHERE (`ID` = $oldNameID)");
-        echo "veikia sita saka";
-        //header("location: http://localhost/SPRINT2PHPsenas/?a=darbuotojai");
-    } else {
-        mysqli_query($connect, "INSERT INTO `darbuotojai`.`projektai` (`Projektai`) VALUES ('$project')");
-        //header("location: http://localhost/SPRINT2PHPsenas/?a=projektai");
-    }
+
+if (isset($name)) {
+    mysqli_query($connect, "UPDATE `darbuotojai`.`employees` 
+    SET `Name` = '$name' WHERE (`ID` = $id)");
+    header("location: http://localhost/SPRINT2PHPsenas/?a=darbuotojai");
+} else {
+    mysqli_query($connect, "INSERT INTO `darbuotojai`.`projects` (`Project`) VALUES ('$project')");
+    header("location: http://localhost/SPRINT2PHPsenas/?a=projektai");
 }
---
+++ update.php
@@ -0,0 +1,30 @@
+<?php
+require_once './config/connect.php';
+
+$id = $_GET['id'];
+$item_to_update = mysqli_query($connect, "SELECT * FROM darbuotojai.employees
+                                            where ID = '$id';");
+$empl = mysqli_fetch_assoc($item_to_update);
+print_r($empl);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Update</title>
+</head>
+
+<body>
+    <div class="container center_div">
+        <h4>Update ITEM</h4>
+        <form action="./functions/update.php" method="post">
+            <input type="hidden" name="id" value="<?= $empl['ID'] ?>">
+            <input class="center" type="text" name="name" value="<?= $empl['Name'] ?>">
+            <button type="submit">Update</button>
+        </form>
+    </div>
+</body>
+
+</html>
